6630cbd9072d74484dd28656a1fa18d2040bcf7c
SamP-S <sam@southcoaststone.com>
2024-12-05 14:45:37 +0000

    setup urls

--- django_rock/boq_assistant/urls.py
+++ django_rock/boq_assistant/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import MyView
+from .views import MyView, MCFCreateView, MCFListView, MCFDetailView, MCFUpdateView, MCFDeleteView
 
 urlpatterns = [
-    path('', MyView.as_view(), name='my_view')
+    path('testing/', MyView.as_view(), name='my_view'),
+    path('', MCFListView.as_view(), name='mcf_list'),
+    path('create/', MCFCreateView.as_view(), name='mcf_create'),
+    path('tasks/<int:pk>/', MCFDetailView.as_view(), name='mcf_detail'),
+    path('tasks/<int:pk>/update/', MCFUpdateView.as_view(), name='mcf_update'),
+    path('tasks/<int:pk>/delete/', MCFDeleteView.as_view(), name='mcf_delete'),
 ]
