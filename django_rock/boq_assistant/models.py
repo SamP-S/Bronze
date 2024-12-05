a97350de401fea2cf0ada982288ece68a759d16d
SamP-S <sam@southcoaststone.com>
2024-12-05 14:45:20 +0000

    update mcf model

--- django_rock/boq_assistant/models.py
+++ django_rock/boq_assistant/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
-class MaxCutFileModel(models.Model):
+class MCFModel(models.Model):
     mc_file = models.FileField("maxcut_file")
-    date = models.DateTimeField("upload_datetime")
+    datetime = models.DateTimeField("upload_datetime")
+    msg = models.TextField("generator_message")
     gen_file = models.FileField("generated_file")
     
-class QuoteFileModel(models.Model):
-    quote_file = models.FileField("quote_file")
-    date = models.DateTimeField("upload_datetime")
-    gen_file = models.FileField("generated_file")
+# class QuoteFileModel(models.Model):
+#     quote_file = models.FileField("quote_file")
+#     date = models.DateTimeField("upload_datetime")
+#     gen_file = models.FileField("generated_file")
     
