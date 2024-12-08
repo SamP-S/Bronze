16ee269447902d9a046a8fbe8a9b7ee7f97afae7
SamP-S <sam.ps1@outlook.com>
2024-12-08 23:48:47 +0000

    add static dir to config/settings.py

--- django_rock/config/settings.py
+++ django_rock/config/settings.py
@@ -120,8 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_ROOT = 'static/media'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/static/static_files'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_ROOT = '/static/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
