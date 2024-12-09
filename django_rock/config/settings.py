28afb548bd3f4f514bac2b03db01876a6faa4235
SamP-S <sam@southcoaststone.com>
2024-12-09 15:12:25 +0000

    fix media root file path

--- django_rock/config/settings.py
+++ django_rock/config/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/static_files'
+STATIC_ROOT = 'static/static_files'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-MEDIA_ROOT = '/static/media'
+MEDIA_ROOT = 'static/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
