ad7efaa8caa118a841fcb0c3da3fc417f64d685d
SamP-S <sam@southcoaststone.com>
2024-12-18 13:54:27 +0000

    migrate scs as root package for better package structure; fix broken package pathing

---
+++ scs/scs/__init__.py
@@ -0,0 +1,3 @@
+from .scs import hello_world
+from . import converters
+from . import auto_mail
--- scs/setup.py
+++ scs/setup.py
@@ -24,3 +24,4 @@ setup(
         "google-api-python-client", "google-auth-httplib2", "google-auth-oauthlib",
     ],
 )
+
