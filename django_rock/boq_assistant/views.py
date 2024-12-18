f715257852899963a054b05ff4e054f00826b8cb
SamP-S <sam@southcoaststone.com>
2024-12-18 14:55:19 +0000

    boq apply conversion

--- django_rock/boq_assistant/views.py
+++ django_rock/boq_assistant/views.py
@@ -7,6 +7,7 @@ from django.urls import reverse_lazy
 import os
 from .models import MCFModel, QFModel
 from .forms import MCFForm, QFForm
+import scs
 
 def my_view(request):
     return HttpResponse("My view")
@@ -15,21 +16,18 @@ def my_view(request):
 # i.e. <MEDIA_ROOT>"/dir/my_file.txt"
 # returns valid_path, original filename
 def get_valid_upload_path(path):
-    filename = os.path.basename(path)
+    # match django's filename auto formatting
+    filename = os.path.basename(path).replace(" ", "_")
     parent_dir = os.path.dirname(path)
+    format_path = os.path.join(parent_dir, filename)
     
     # ensure necessary subdir exists
     if not os.path.exists(os.path.join(settings.MEDIA_ROOT, parent_dir)):
         os.makedirs(os.path.join(settings.MEDIA_ROOT, parent_dir), exist_ok=True)
     
-    print(f"path: {path}")
-    print(f"path type: {type(path)}")
-    print(f"dirname: {parent_dir}")
-    print(f"basename: {filename}")
-    
     # catch guard for existing path
-    if not os.path.exists(os.path.join(settings.MEDIA_ROOT, path)):
-        return path, filename
+    if not os.path.exists(os.path.join(settings.MEDIA_ROOT, format_path)):
+        return format_path, filename
     
     # file exists, generate new path sequentially
     # TODO: use random str generation for better performance
@@ -98,24 +96,40 @@ class QFCreateView(CreateView):
     success_url = reverse_lazy("qf_list")
     
     ### TODO: implement file path validation to ensure existing files dont get deleted
+    # TODO: test what must be relative to root and what must be relative to MEDIA_ROOT
+    # TODO: consider organising uploaded files by type->user->year->month
     def form_valid(self, form):
         obj = form.save(commit=False)
         
         ori_path = os.path.join("qf", obj.q_file.name)
         print(f"original path = {ori_path}")
-        new_path, ori_filename = get_valid_upload_path(ori_path)
+        obj.filename = os.path.basename(ori_path).replace(".xlsx", ".zip")
         
+        new_path, ori_filename = get_valid_upload_path(ori_path)
         obj.q_file.name = new_path
         
-        path = settings.MEDIA_ROOT + "/test.txt"
-        with open(path, "wt") as f:
-            f.write("1234\n")
+        # must save first to force uploaded file to download
+        obj.save()
+        
+        # generate converted file
+        rel_root_path = os.path.join(settings.MEDIA_ROOT, new_path)
+        print(f"rel_root_path: {rel_root_path}")
+        print(f"list dirs:\n {os.listdir(os.path.dirname(rel_root_path))}")
+        
+        print(f"path: {rel_root_path}")
+        gen_path = scs.converters.quote_to_maxcut(rel_root_path)
+        print(f"gen_path: {gen_path}")
+        print(f"gen_path type: {type(gen_path)}")
         
         # Save the new file path to gen_file
-        obj.gen_file.name = path
+        print(f"media_root: {settings.MEDIA_ROOT}")
+        print(f"cwd: {os.getcwd()}")
+        print(f"cwd type: {type(os.getcwd())}")
+        gen_path = gen_path.replace(os.getcwd(), "")
+        print(f"gen_path: {gen_path}")
+        obj.gen_file.name = gen_path
         obj.warnings = "No warnings"
         print(f"generated file: {obj.gen_file}")
-        obj.save()
         
         return super().form_valid(form)
     
